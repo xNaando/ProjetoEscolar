584b9e0a62fed5f9862cae606de6611f1856c9b2
Fernando Henrique <xnandoh@gmail.com>
2025-05-06 21:25:11 -0300

    qwe

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configuração do CORS e middleware para JSON
 app.use(cors());
